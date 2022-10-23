4f2afd239a8d1855ff5d1dd469d4483d8430bbbe
wathone <mg.wathonekhant@gmail.com>
2022-10-23 23:35:33 +0630

    block

---
+++ src/components/Block.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="block" v-if="showblock" @click="stopTimer">Play {{delay}}</div>
+</template>
+
+<script>
+export default {
+    props:['delay'],
+    data(){
+        return{
+            showblock:false,
+            score:0,
+            timer:null
+        }
+    },
+    mounted(){
+        setTimeout(()=>{
+            this.showblock=true;
+            this.startTimer()
+        },this.delay);
+    },
+    methods:{
+        startTimer(){
+            this.timer=setInterval(() => {
+                this.score+=50;
+            }, 50);
+        },
+        stopTimer(){
+            clearInterval(this.timer);
+            this.$emit("endGame",this.score)
+        }
+    }
+}
+</script>
+
+<style>
+.block{
+    width: 400px;
+    height: 300px;
+    background-color: aqua;
+    margin: 20px auto;
+    padding-top: 30px;
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>Result is {{score}}ms</p>
+  <div v-if="score<250">
+      <p>you are to fast</p>
+  </div>
+  <div v-else-if="score>250 && score<500">
+      <p>you are normal</p>
+  </div>
+  <div v-else>
+      <p>you have to try more</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['score']
+}
+</script>
+
+<style>
+
+</style>
